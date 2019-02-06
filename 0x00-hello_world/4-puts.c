746d6f56c4e0b7b919fa813baf7abb655dc5526b
JohnCook17 <655@holbertonschool.com>
2019-02-06 22:02:06 +0000

    assignment 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str[50];
+
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
+
+	puts(str);
+
+	return(0);
+}
