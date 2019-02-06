37842f4453766aafad40ed7bec3894ff71c92b79
JohnCook17 <655@holbertonschool.com>
2019-02-06 22:39:16 +0000

    assignment 04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
+/* prints text in str */
 #include <stdio.h>
 #include <string.h>
-
-int main()
+/**
+ * main
+ */
+int main(void)
 {
 	char str[50];
 
 	strcpy(str, "\"Programming is like building a multilingual puzzle");
 
 	puts(str);
-
+/**
+ * returns 0 cause it does almost nothing
+ */
 	return (0);
 }
