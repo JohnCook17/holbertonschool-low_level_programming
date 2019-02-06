699bc35f46b56f17bf20fecdc3c54af041498962
JohnCook17 <655@holbertonschool.com>
2019-02-06 22:55:04 +0000

    assignment 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/* prints text in str */
+#include <stdio.h>
+/**
+ * main - The main function
+ * @void: void
+ *
+ * Description: main is where all the code is called.
+ * section header: prints the text.
+ * Return: returns 0 as instructed.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
