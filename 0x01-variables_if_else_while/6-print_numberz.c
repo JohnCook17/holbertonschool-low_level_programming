f404530833fc8639717b6f21368044c1763608fc
JohnCook17 <655@holbertonschool.com>
2019-02-07 21:02:45 +0000

    forgot comments

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-
+/**
+ * main - prints numbers
+ * @void: void
+ *
+ * Description: prints numbers 0-9
+ * section header: stdio
+ * Return: returns 0
+ */
 int main(void)
 {
 	int i;
 
 	i = 0;
 
-	while(i != 10)
+	while (i != 10)
 	{
-		putchar(i);
+		putchar (i);
 		i++;
 	}
 
-	return(0);
+	return (0);
 }
