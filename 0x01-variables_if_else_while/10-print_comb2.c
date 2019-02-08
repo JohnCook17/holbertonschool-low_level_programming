20b550f7f6071a17f8e7474d6e75843bd6f65fe9
JohnCook17 <655@holbertonschool.com>
2019-02-08 01:14:20 +0000

    assignment 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print alpha
+ * @void: void
+ *
+ * Description: A program to print the alphabet
+ * section header: stdio
+ * Return: returns 0
+ */
+
+int main(void)
+{
+
+	int i;
+	int n;
+
+	i = 48;
+	n = 48;
+
+	while (n != 58)
+	{
+
+		while (i != 58)
+		{
+			putchar(n);
+			putchar(i);
+			if (i != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			i++;
+		}
+		if (i = 57)
+		{
+			i = 48;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
