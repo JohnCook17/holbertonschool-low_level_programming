029c149d4b4a9ab4cee24d9c43a1239cbda06dc5
JohnCook17 <655@holbertonschool.com>
2019-02-10 02:54:14 +0000

    added descriptions for functions

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,14 +4,26 @@
  * main - A program to print alpha
  * @void: void
  *
- * Description: A program to print the alphabet
+ * Description: A program to print the combinations of 0-9 2 times
  * section header: stdio
  * Return: returns 0
  */
+
 int main(void)
 {
-
-
+/**
+ * comb_until - combines until
+ * @arr: an arry with the values 0-9 in ascii code
+ * @n: size of input array
+ * @r: size of combination to be printed
+ * @index: current index in data[]
+ * @data: temp array to store current combination
+ * @i: index of the current element in arr[]
+ *
+ * Description: A function to combine and print 0-9 n digits long
+ * section header: stdio
+ * Return: returns nothing, might make it void later
+ */
 	void comb_until(int arr[], int n, int r, int index, int data[], int i)
 	{
 		int j;
@@ -41,14 +53,27 @@ int main(void)
 		comb_until(arr, n, r, index + 1, data, i + 1);
 		comb_until(arr, n, r, index, data, i + 1);
 	}
-
+/**
+ * print_comb - Prints combinations.
+ * @arr: Input array
+ * @n: size of input array
+ * @r: size of combination to be printed
+ *
+ * Description: Prints the combinations of 0-9 r times
+ * Return: no return, might want to fix this later...
+ */
 	void print_comb(int arr[], int n, int r)
 	{
 		int data[r];
 
 		comb_until(arr, n, r, 0, data, 0);
 	}
-
+/**
+ * results - A function to call other functions to get results
+ *
+ * Description: a function to call other functions to get results
+ * Return: 0
+ */
 	int results(void)
 	{
 		int arr[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
@@ -58,6 +83,7 @@ int main(void)
 		print_comb(arr, n, r);
 		return (0);
 	}
+
 	results();
 	putchar('\n');
 	return (0);
