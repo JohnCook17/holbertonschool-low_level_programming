6e10cc29fffd9e101134d1cb0c210a9c5558a9be
JohnCook17 <655@holbertonschool.com>
2019-02-13 02:20:35 +0000

    assignment 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - adds two numbers
+ *@a: a number
+ *@b: a number
+ *
+ *Description: adds two numbers, does not check if they are numbers though
+ *Return: returns the sum of two numbers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
