18e723ade3b65c91e8bc3159bdf2efa44b42eafa
JohnCook17 <655@holbertonschool.com>
2019-02-19 19:34:12 +0000

    7-puts_half.c prints half a string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * puts_half - puts the last half of a string
+ * @str: string to put half of
+ *
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	while (j <= i)
+	{
+		if (j >= i / 2)
+		{
+			_putchar(str[j]);
+		}
+		j++;
+	}
+	_putchar('\n');
+}
