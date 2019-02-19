911353bbc2517f7125a39a014b08f4bcaf61f504
JohnCook17 <655@holbertonschool.com>
2019-02-19 22:16:45 +0000

    9-strcpy.c, coppies a string into another string

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strcpy - coppies a string to another string
+ * @dest: destination
+ * @src: sorce
+ *
+ * Return: returns the coppied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	char c;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	while (j < i)
+	{
+		c = src[j];
+		dest[j] = c;
+		j++;
+	}
+	return (dest);
+}
