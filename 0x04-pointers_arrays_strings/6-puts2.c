7b898f07098a4d4c29787506497a0d87764b1eea
JohnCook17 <655@holbertonschool.com>
2019-02-19 18:58:03 +0000

    6-puts2.c puts every other char

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * puts2 - puts every other char of string
+ * @str: string
+ *
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
