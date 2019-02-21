6966efe2cf68a72debbcb2268818dc24fe8782ae
JohnCook17 <655@holbertonschool.com>
2019-02-21 20:09:19 +0000

    4, reverses an array

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * rev - reverse a array
+ * @a: array
+ * @n: size of array
+ *
+ * Return: to keep in the array, if no return segmentation fault
+ */
+void rev(int *a, int n)
+{
+	static int i;
+	static int j;
+	int c;
+
+	c = a[i];
+	if (i == n)
+	{
+		return;
+	}
+	i++;
+	rev(a, n);
+	i--;
+	a[j] = c;
+	j++;
+}
+/**
+ * reverse_array - reverse an array
+ * @a: array
+ * @n: size of array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	rev(a, n);
+}
