37c1509cc3e34485caae8c99fb543609a14119a4
JohnCook17 <655@holbertonschool.com>
2019-02-22 07:04:03 +0000

    2-strncpy.c, coppies n bytes of a string

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,33 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * _strncpy - coppies a string
  * @dest: destination string
  * @src: source string
  * @n: how much to coppy
  *
+ * Description: I addmit that I had to look at the man page source code for this
+ * I could not figure out the null padding on my own, so I used google, but I
+ * thoughly understand it now.
  * Return: returns the modified dest string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	char *d = dest;
+	char *s = src;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	if (n != 0)
 	{
-		dest[i] = src[i];
+		do {
+			if ((*d++ = *s++) == 0)
+			{
+				while (--n != 0)
+				{
+					*d++ = 0;
+				}
+				break;
+			}
+		} while (--n != 0);
 	}
 	return (dest);
 }
