efad4494f52567b5ea7b405e9f48874f2db24782
JohnCook17 <655@holbertonschool.com>
2019-02-25 23:14:18 +0000

    5-strstr.c, find the str in str and return the start of str in str

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * _strstr - finds the needle in the haystack
+ * @haystack: haystack string
+ * @needle: needle string
+ *
+ * Return: return beginning of haystack where needle was found or if no match
+ * return null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int match = 1;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				match = 0;
+				continue;
+			}
+			if (haystack[i] != needle[j])
+			{
+				match = 1;
+			}
+		}
+		if (match == 0)
+		{
+			return(&haystack[i - j + 1]);
+		}
+	}
+	return ('\0');
+}
