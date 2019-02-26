9318d0ac7d4756667b31e45553cc4a86cc2d6dad
JohnCook17 <655@holbertonschool.com>
2019-02-26 04:18:55 +0000

    8-print_diagsums.c, prints diagonal sums of an array

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - adds the diagonal summs of an array
+ * @a: array
+ * @size: size of array
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	int row;
+	int *num;
+	int numsum = 0;
+
+	for (row = 0; row < size; row++)
+	{
+		num = a + (size + 1) * row;
+		numsum += *num;
+	}
+	printf("%d, ", numsum);
+	for (row = 1, numsum = 0; row - 1 < size; row++)
+	{
+		num = a + (size - 1) * row;
+		numsum += *num;
+	}
+	printf("%d \n", numsum);
+}
