0885aa715d8394b098e553096f907feea216d052
JohnCook17 <655@holbertonschool.com>
2019-02-26 05:03:04 +0000

    9-set_string.c, sets a string pointer to another string

--- 0x06-pointers_arrays_strings/9-set_string.c
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
-#include <stdio.h>
+/**
+ * set_string - sets the pointer of one string to another
+ * @s: string pointer
+ * @to: string to change
+ *
+ */
 
 void set_string(char **s, char *to)
 {
