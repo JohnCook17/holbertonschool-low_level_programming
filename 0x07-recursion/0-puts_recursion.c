1f8290cfb888f5d0414c98daab3d6ae624b7ce88
JohnCook17 <655@holbertonschool.com>
2019-02-27 21:01:45 +0000

    put a helper function in 0

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -1,4 +1,19 @@
 #include "holberton.h"
+
+void helper(s)
+{
+        static int i;
+
+        if (s[i] == '\0')
+        {
+                _putchar('\n');
+                return;
+        }
+        _putchar(s[i]);
+        i++;
+        _puts_recursion(s);
+}
+
 /**
  * _puts_recursion - puts the string followed by a newline
  * @s: string
@@ -7,14 +22,5 @@
  */
 void _puts_recursion(char *s)
 {
-	static int i;
-
-	if (s[i] == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(s[i]);
-	i++;
-	_puts_recursion(s);
+	helper(s);
 }
