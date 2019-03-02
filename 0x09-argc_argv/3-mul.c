122f846dfaa3e33293cb73bd44eb920db606e6c7
JohnCook17 <655@holbertonschool.com>
2019-03-02 21:58:03 +0000

    0-4

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -14,6 +14,11 @@ int main(int argc, char *argv[])
 {
 	int num1, num2;
 
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	UNUSED(argc);
