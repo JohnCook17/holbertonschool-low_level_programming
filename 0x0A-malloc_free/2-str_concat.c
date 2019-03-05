414a2bad55b9817ea0d4650730ca376d76c64e50
JohnCook17 <655@holbertonschool.com>
2019-03-05 21:00:40 +0000

    made betty compliant

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,28 +1,34 @@
 #include <stdlib.h>
 #include "holberton.h"
-
+/**
+ * str_concat - concatinates two strings
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: returns null if error, pointer to start of cat string if pass
+ */
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
 	int i = 0;
 	int j = 0;
 
-	p = malloc(sizeof(s1) + sizeof(s2) -1);
+	p = malloc(sizeof(s1) + sizeof(s2) - 1);
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	while(s1[i] != '\0')
+	while (s1[i] != '\0')
 	{
 		p[i] = s1[i];
 		i++;
 	}
-	while(s2[j] != '\0')
+	while (s2[j] != '\0')
 	{
 		p[i] = s2[j];
 		i++;
 		j++;
 	}
-	p[i+1] = '\0';
+	p[i + 1] = '\0';
 	return (p);
 }
