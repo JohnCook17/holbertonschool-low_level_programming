414931af8665b00069fc2904042d70d6155f414a
JohnCook17 <655@holbertonschool.com>
2019-03-05 20:26:44 +0000

    fixed if malloc returns null

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "holberton.h"
-
+/**
+ * create_array - creates an array and inits values to char
+ * @size: size of array to make
+ * @c: char to init to
+ *
+ * Return: returns pointer to start of array
+ */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
@@ -11,7 +17,11 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	p = (char*)malloc( sizeof(char) * size);
+	p = (char *)malloc(sizeof(char) * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
