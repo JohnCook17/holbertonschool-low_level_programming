f95ce0ff277d68f6e08041bb54cb336aa2814f56
JohnCook17 <655@holbertonschool.com>
2019-03-06 00:37:03 +0000

    3-alloc_grid.c, makes a grid n by n

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * alloc_grid - allocates an arry of width * height to 0
+ * @width: width
+ * @height: height
+ *
+ * Return: NULL if error, pointer to array if not
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int j;
+	int **p;
+	int k;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+/* malloc starts here */
+	p = (int **)malloc(width * sizeof(int *));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < width; k++)
+	{
+		p[k] = (int *)malloc(height * sizeof(int));
+		if (p[k] == NULL)
+		{
+			for ( ; k > 0; k--)
+			{
+				free(p[k]);
+			}
+			free(p);
+		}
+	}
+/* malloc ends here */
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
