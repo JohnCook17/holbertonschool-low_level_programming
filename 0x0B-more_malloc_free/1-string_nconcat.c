85e19011866f3ab9632a519d6aa2efcfc9817d0e
JohnCook17 <655@holbertonschool.com>
2019-03-07 20:40:08 +0000

    1-string_nconcat.c, concatinates n chars of string two to string one

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int len1 = 0;
 	char *p;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (s1[i] != '\0')
 	{
 		len1++;
