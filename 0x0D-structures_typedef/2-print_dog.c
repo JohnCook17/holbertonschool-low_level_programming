dcee7605c66ee995fc7659468b3dc5ca2c7068a7
JohnCook17 <655@holbertonschool.com>
2019-03-11 23:11:13 +0000

    fixed how it handles floats

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 #include "dog.h"
 
 /**
@@ -15,10 +14,7 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (isnan(d->age))
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
