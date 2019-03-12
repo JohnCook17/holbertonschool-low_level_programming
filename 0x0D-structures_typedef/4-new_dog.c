8c249ac44b485177f39ff57d0f134d2d4f35ed81
JohnCook17 <655@holbertonschool.com>
2019-03-12 00:00:16 +0000

    added free if fail

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -29,6 +29,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	float newage;
 	dog_t *newdog;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 	{
