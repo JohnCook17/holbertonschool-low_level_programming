6fbed241ffefb92cd96e0266de231fc9e473f8a2
JohnCook17 <655@holbertonschool.com>
2019-03-16 02:43:15 +0000

    3-print_all.c, prints chars, ints, floats, and strings

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * printchar - prints a char
+ * @s: va_list
+ */
+void printchar(va_list s)
+{
+	printf("%c", va_arg(s, int));
+}
+/**
+ * printint - prints an int
+ * @s: va_list
+ */
+void printint(va_list s)
+{
+	printf("%i", va_arg(s, int));
+}
+/**
+ * printfloat - prints a float
+ * @s: va_list
+ */
+void printfloat(va_list s)
+{
+	printf("%lf", va_arg(s, double));
+}
+/**
+ * printstring - prints a string
+ * @s: va_list
+ */
+void printstring(va_list s)
+{
+	if (s == NULL)
+		printf("(nil)");
+	printf("%s", va_arg(s, char *));
+}
+/**
+ * print_all - prints chars, ints floats, strings
+ * @format: type of arg
+ */
+void print_all(const char * const format, ...)
+{
+/**
+ * struct form - matches format and allows the call of the proper func
+ * @t: char type
+ * @func: function to call
+ */
+	struct form
+	{
+		char t;
+		void (*func)(va_list);
+	};
+	struct form type[] = {
+		{'c', printchar},
+		{'i', printint},
+		{'f', printfloat},
+		{'s', printstring}
+	};
+	int i = 0;
+	va_list statment;
+	int j = 0;
+
+	va_start(statment, format);
+	while (format[j] != '\0')
+	{
+		while (i < 4)
+		{
+			if (format[j] == type[i].t)
+			{
+				type[i].func(statment);
+				if (format[j + 1] != '\0')
+					printf(", ");
+			}
+			i++;
+		}
+		i = 0;
+		j++;
+	}
+	va_end(statment);
+	printf("\n");
+}
