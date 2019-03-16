e11799e4c40708600973c62fbbd0ba9f0d9f57a4
JohnCook17 <655@holbertonschool.com>
2019-03-16 20:23:46 +0000

    variadic_functions.h, included the stdarg.h lib

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -5,6 +5,17 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+/**
+ * struct form - matches format and allows the call of the proper func
+ * @t: char type
+ * @func: function to call
+ */
+struct form
+{
+	char t;
+	void (*func)(va_list);
+};
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
