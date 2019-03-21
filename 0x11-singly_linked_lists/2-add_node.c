df9fc5829cbeff5ba817e99bb919c2acb5f6bbd5
JohnCook17 <655@holbertonschool.com>
2019-03-21 22:55:48 +0000

    2-add_node.c

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node to the linked list at the start of the list
+ * @head: the head of the list
+ * @str: the value of the element of the list
+ *
+ * Return: new_node, or NULL if malloc fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
