931b288ae4189d8b52aae06cc19e2280952ce67a
JohnCook17 <655@holbertonschool.com>
2019-03-21 23:33:43 +0000

    2-add_node.c, could not use strlen, made _strlen

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -1,6 +1,20 @@
 #include "lists.h"
 
 /**
+ * _strlen - finds the len of a str
+ * @str: string to find len of
+ *
+ * Return: i, len of string
+ */
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+/**
  * add_node - adds a new node to the linked list at the start of the list
  * @head: the head of the list
  * @str: the value of the element of the list
@@ -10,11 +24,11 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
-	char *s = strdup(str);
+
 	if (new_node == NULL)
 		return (NULL);
-	new_node->str = s;
-	new_node->len = strlen(str);
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
