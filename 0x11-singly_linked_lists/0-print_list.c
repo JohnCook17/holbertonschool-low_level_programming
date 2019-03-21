5b638a77002222c43e8181bf8988a33a7c0eceb3
JohnCook17 <655@holbertonschool.com>
2019-03-21 22:31:09 +0000

    0-print_list.c, prints a linked list

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - print the values of a linked list, and the length of the values
+ * @h: the list to print
+ *
+ * Return: elements, number of elements in a list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *temp = h;
+	int elements = 0;
+
+	while (temp)
+	{
+		printf("[%i] %s\n", temp->len, temp->str);
+		temp = temp->next;
+		elements++;
+	}
+	return (elements);
+}
