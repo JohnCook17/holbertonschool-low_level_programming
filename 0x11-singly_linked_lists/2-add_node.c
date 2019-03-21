6f8f31a84002a67b5ec822bbfb21876cd17b5751
JohnCook17 <655@holbertonschool.com>
2019-03-21 23:24:29 +0000

    2-add_node, stored a local copy of string

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -10,12 +10,12 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
-
+	char *s = strdup(str);
 	if (new_node == NULL)
 		return (NULL);
-	new_node->str = strdup(str);
+	new_node->str = s;
 	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-	return (head);
+	return (new_node);
 }
