22b976230e2574678cb5a771ca2b561560e1d4c4
JohnCook17 <655@holbertonschool.com>
2019-03-25 17:14:49 +0000

    5-free_listint2.c, frees a linked list and sets head to null

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list, and sets the head to null
+ * @head: start of linked list to free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+	listint_t *next;
+
+	current = *head;
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	head = NULL;
+}
