baffea693a61ec4e2e4ace8e4db21186a6554834
JohnCook17 <655@holbertonschool.com>
2019-03-25 16:22:16 +0000

    0-print_listint.c, prints ints in a linked list

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints a linked list and returns the number of nodes.
+ * @h: linked list to print.
+ *
+ * Return: The number of nodes printed.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		printf("%i\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
