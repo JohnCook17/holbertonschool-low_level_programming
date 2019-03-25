e8c418ddee6978ece8f42e703526c98ab7efc32e
JohnCook17 <655@holbertonschool.com>
2019-03-25 16:23:16 +0000

    1-listint_len.c, finds the length of a linked list and returns the length

---
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - finds the length of a linked list and returns the length
+ * @h: linked to to find the length of.
+ *
+ * Return: the length of the linked list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
