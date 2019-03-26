d24126b7a7cf77e8b94c0af7fe8b94bbc5aadd77
JohnCook17 <655@holbertonschool.com>
2019-03-26 22:28:09 +0000

    fixed a style error

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 	prev = *head;
-	while (i + 1< idx)
+	while (i + 1 < idx)
 	{
 		prev = prev->next;
 		if (prev == NULL)
