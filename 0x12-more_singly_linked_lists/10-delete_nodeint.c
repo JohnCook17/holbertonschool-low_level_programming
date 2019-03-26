000e51023eefeb251c8d36cc5de5241d91d1b8c5
JohnCook17 <655@holbertonschool.com>
2019-03-26 22:49:13 +0000

    index was off

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(current);
 		return (1);
 	}
-	while (i < index - 1 && current)
+	while (i < index && current)
 	{
 		prev = current;
 		current = current->next;
