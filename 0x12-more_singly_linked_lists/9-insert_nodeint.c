9b246880ba01f88a6647de49b6ecf07704092208
JohnCook17 <655@holbertonschool.com>
2019-03-26 22:25:58 +0000

    will now insert node to 0 index

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,23 +10,33 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
+	unsigned int i = 0;
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *prev;
-
 
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (idx == 0)
+	if (head == NULL && idx != 0)
+		return (NULL);
+	if (head == NULL && idx == 0)
 	{
-		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	if (head && idx == 0)
+	{
+		prev = *head;
+		*head = new_node;
+		new_node->next = prev;
 		return (new_node);
 	}
 	prev = *head;
-	while (i < idx)
+	while (i + 1< idx)
 	{
 		prev = prev->next;
+		if (prev == NULL)
+			break;
 		i++;
 	}
 	new_node->next = prev->next;
