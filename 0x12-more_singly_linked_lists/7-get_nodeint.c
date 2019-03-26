52402fe75cb8e3ac0de15c00b7178a27bf6f8e01
JohnCook17 <655@holbertonschool.com>
2019-03-26 17:27:55 +0000

    7-get_nodeint.c, check if i is less then index after loop

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -12,12 +12,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *temp = head;
 
-	if (temp == NULL)
+	if (head == NULL)
 		return (NULL);
 	while (i < index && head)
 	{
 		temp = temp->next;
 		i++;
 	}
+	if (i < index)
+		return (NULL);
 	return (temp);
 }
