8af92a40b51fe0d611be703ffefe705387ac3843
JohnCook17 <655@holbertonschool.com>
2019-03-28 21:24:13 +0000

    3-set_bit.c, sets the nth bit to on

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * set_bit - sets the nth bit to on
+ * @n: number to set nth bit of
+ * @index: nth bit to set
+ *
+ * Return: the value with the nth bit turned on, or -1 if it fails
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 31)
+		return (-1);
+	return (*n |= 1UL << index);
+}
