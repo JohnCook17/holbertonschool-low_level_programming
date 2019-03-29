01d357b8869e5143a87d58bf5a7a41ee2ef09ca1
JohnCook17 <655@holbertonschool.com>
2019-03-29 05:08:04 +0000

    100-get_endianness.c, fixed a style error

--- 0x13-bit_manipulation/100-get_endianness.c
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
 
 /**
+ * get_endianness - determines the memory type on the current machine
+ *
+ * Return: returns 1 if little endian, or 0 if big endian.
  */
 
 int get_endianness(void)
 {
 	unsigned int i = 1;
-	char *c = (char *) & i;
+	char *c = (char *) &i;
 
 	if (*c)
 		return (1);
