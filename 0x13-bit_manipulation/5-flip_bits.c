1c783996269223a96f3b59c14e29e38bf55425dd
JohnCook17 <655@holbertonschool.com>
2019-03-29 00:26:03 +0000

    5-flip_bits.c, fixed a style error

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int count = 0;
 	int temp;
 
-	for (temp = n^m; temp != 0; temp = temp >> 1)
+	for (temp = n ^ m; temp != 0; temp = temp >> 1)
 		count += temp & 1;
-	return count;
+	return (count);
 }
