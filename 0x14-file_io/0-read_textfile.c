bb78a9170285b06e2ea81d9c00ac12e3243e72e9
JohnCook17 <655@holbertonschool.com>
2019-04-02 21:30:07 +0000

    0-read_textfile.c, accounted for a null case

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -8,13 +8,14 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int o = open(filename, O_RDONLY);
+	int o;
 	int w = 0;
 	char buf[1024];
 	int r;
 
 	if (filename == NULL || letters == 0)
 		return (0);
+	o = open(filename, O_RDONLY);
 	if (o == -1)
 		return (0);
 	r = read(o, buf, letters);
