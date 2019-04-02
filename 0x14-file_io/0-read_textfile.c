e220f36dc5d1de93ebf7e6c4108c9e04332ab053
JohnCook17 <655@holbertonschool.com>
2019-04-02 21:12:37 +0000

    0-read_textfile.c, reads a text file and puts the output to stdout

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * read_textfile -
- *
- * Return: 
+ * read_textfile - reads text from a file, puts it to stdout
+ * @filename: filename
+ * @letters: size of file to print
+ * Return: 0 if fail, else the number of chars printed.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -14,12 +15,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (o == -1)
 		return (0);
-	if (buf == NULL)
-		return (0);
 	r = read(o, buf, letters);
 	if (r <= 0)
 		return (0);
 	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1)
+		return (0);
 	close(o);
 	return (w);
 }
