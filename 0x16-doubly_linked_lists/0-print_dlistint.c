a5d6c24563d440671f3c27c524205562bfa35076
JohnCook17 <655@holbertonschool.com>
2019-05-04 19:42:20 +0000

    0-print_dlistint.c, added function description

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * print_dlistint -
+ * print_dlistint - prints a doubly linked list
+ * @h: the head of the doubly linked list
  *
- * Return: 
+ * Return: the number of elemets in the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
