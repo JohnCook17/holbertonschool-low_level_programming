d18578e814df2c8fcd20280e9a561c8364ce0b2a
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-02 15:45:27 -0700

    added a condition if malloc fails, betty style

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,12 +1,18 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array in the hash table.
+ * Return: NULL if fail, else ht
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht = malloc(sizeof(hash_table_t));
-    hash_node_t *ar[size];
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_node_t *ar[size];
 
-    ht->size = size;
-    ht->array = ar;
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = ar;
 
-    return (ht);
+	return (ht);
 }
