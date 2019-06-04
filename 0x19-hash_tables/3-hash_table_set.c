6de9d693a0c999c9ab9d84174801e492074e8420
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-03 20:58:31 -0700

    3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -2,7 +2,38 @@
 
 void add_node(hash_table_t *ht, hash_node_t *node)
 {
-    unsigned long int i = key_index(node->key, ht->size);
+    unsigned long int i = key_index((unsigned char *)node->key, ht->size);
+    hash_node_t *temp = ht->array[i];
+
+    if (ht->array[i] != NULL)
+    {
+        temp = ht->array[i];
+        while (temp != NULL)
+        {
+            if (strcmp(temp->key, node->key) == 0)
+                break;
+            temp = temp->next;
+        }
+        if (temp == NULL)
+        {
+            node->next = ht->array[i];
+            ht->array[i] = node;
+        }
+        else
+        {
+            free(temp->value);
+            temp->value = strdup(node->value);
+            free(node->value);
+            free(node->key);
+            free(node);
+        }
+        
+    }
+    else
+    {
+        node->next = NULL;
+        ht->array[i] = node;
+    }
     
 }
 
