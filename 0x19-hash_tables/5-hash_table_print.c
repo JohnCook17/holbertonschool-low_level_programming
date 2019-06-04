bddc64bb0178d863367064e94aa90425c2bce90a
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-04 14:12:24 -0700

    5

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,22 +1,30 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_print - prints a hashtable
+ * @ht: the hashtable
+ */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i = 0;
+	unsigned long int i = 0;
+	int first = 1;
 
-    printf("{");
-    while (i < ht->size)
-    {
-        if (ht->array[i] != NULL)
-        {
-            if (ht->array[i]->key != NULL)
-            {
-                printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-            }
-            if (ht->array[i]->next->key != NULL)
-                printf(",");
-        }
-        i++;
-    }
-    printf("}\n");
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (ht->array[i]->key != NULL)
+			{
+				if (first == 1)
+				{
+					printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+					first = 0;
+				}
+				else
+					printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			}
+		}
+		i++;
+	}
+	printf("}\n");
 }
