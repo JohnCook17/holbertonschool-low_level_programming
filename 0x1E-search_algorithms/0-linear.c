ab7fd653e7279cd21c0028c545fc3f45a9cf7b8a
JohnCook17 <655@holbertonschool.com>
2019-08-27 14:30:19 -0700

    0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search
+ * @array: the array to search in
+ * @size: the size of the array
+ * @value: the value to search for
+ * Return: array index if found, else -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		if (array[i] == value)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		}
+		i++;
+	}
+	return (-1);
+}
